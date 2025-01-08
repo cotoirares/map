f21d35f8d7d9c1e73c712e71f2c87d27d72b8bd4
Rares-Andrei Cotoi <62752604+coty1@users.noreply.github.com>
2025-01-08 13:14:47 +0200

    Added maven for A7

--- ToyLanguageInterpreter/src/Main.java
+++
@@ -1,7 +0,0 @@
-import View.View;
-
-class Main {
-    public static void main(String[] args) {
-        View.main(args);
-    }
-}
---
+++ ToyLanguageInterpreter/src/main/java/Main.java
@@ -0,0 +1,30 @@
+//import View.View;
+
+//class Main {
+  //  public static void main(String[] args) {
+    //    View.main(args);
+    //}
+//}
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
